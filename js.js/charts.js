afbbb6a4c8ea58a5184a860fd1d6e73d07fe684b
twmathieu <85597990+twmathieu@users.noreply.github.com>
2021-09-08 20:05:27 -0500

    Update charts.js

--- js.js/charts.js
+++ js.js/charts.js
@@ -65,39 +65,29 @@ function buildCharts(sample) {
     var result = resultArray[0]
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-    var  ids = result.otu_ids;
-    var labels = result.otu_labels.slice(0, 10).reverse();
-    var values = result.sample_values.slice(0,10).reverse();
-    
+ 
     ;
 
 
     // 7. Create the yticks for the bar chart.
-    // Hint: Get the the top 10 otu_ids and map them in descending order  
-    //  so the otu_ids with the most bacteria are last. 
+   
 
     var yticks = ids.map(sampleObj => "OTU " + sampleObj).slice(0,10).reverse();
 
     console.log(yticks)
 
     // 8. Create the trace for the bar chart. 
-    var barData = [{
-      x: values,
-      y: yticks,
-      type: "bar",
-      orientation: "h",
-      text: labels 
+    
     }];
 
 
     // 9. Create the layout for the bar chart. 
-    var barLayout = {
-      title: "Top 10 Bacteria Cultures Found"
+    
      };
 
 
      // 10. Use Plotly to plot the data with the layout. 
-     Plotly.newPlot("bar", barData, barLayout);
+    
 
 
     });
